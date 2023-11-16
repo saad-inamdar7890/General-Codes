ec3636544780445d9cfd8e5095cee885e40936e7
saad-inamdar7890 <148623594+saad-inamdar7890@users.noreply.github.com>
2023-11-17 00:00:42 +0530

    added insertion and deletion

--- Binary_Tree.c
+++ Binary_Tree.c
@@ -6,6 +6,42 @@ struct Node
     struct Node* left;
     struct Node* right;
 };
+
+struct Node *inOrderPredecessor( struct Node * root)
+{
+    root = root->left;
+    while(root->right !=    NULL)
+    {
+        root = root->right;
+    }
+    return root;
+}
+
+
+struct Node *deleteNode(struct Node *root , int value)
+{
+struct Node* ipre;
+if(root == NULL){
+    return NULL;
+}
+if(root->left == NULL && root->right == NULL){
+    free(root);
+}
+if(value < root->data){
+    deleteNode(root->left, value);
+}
+else if(value > root->data)
+deleteNode(root->left, value);
+else {
+    ipre = inOrderPredecessor(root);
+    root->data = ipre->data;
+    deleteNode(root->left , ipre->data);
+}
+}
+
+
+
+
 struct Node* createnode( int data)
 {
     struct Node *n ;
@@ -19,7 +55,7 @@ struct Node* createnode( int data)
 
 int isBST( struct Node* root)
 {
-    struct Node *prev = NULL;
+     static struct Node *prev = NULL;
     if(root!= NULL )
     {
         if(!isBST(root->left))
@@ -53,33 +89,58 @@ return search(root->right , key);
 }
 
 
+
+void insert(struct Node *root , int key)
+{
+    struct Node *prev = NULL;
+    while(root != NULL)
+    {
+        prev = root;
+        if(key == root->data)
+        return;
+        else if(key < root->data)
+        root = root->left;
+        else
+        root = root->right;
+    }
+    struct Node* new = createnode(key);
+    if(key < prev->data)
+    prev->left = new;
+    else
+    prev->right = new;
+}
+
+
 void transverse(struct Node* root )
 {
 if(root != NULL)
 {
         transverse(root->left);
-    transverse(root->right);
     printf(" %d", root->data);
+        transverse(root->right);
+
 
 }
 };
 
 int main() 
 {
-    struct Node *p = createnode(0);
+    struct Node *p = createnode(4);
         struct Node *p1 = createnode(1);
-    struct Node *p2 = createnode(2);
-    struct Node *p3 = createnode(3);
-    struct Node *p4 = createnode(4);
-        struct Node *p5 = createnode(5);
+    struct Node *p2 = createnode(5);
+    struct Node *p3 = createnode(0);
+    struct Node *p4 = createnode(6);
+        struct Node *p5 = createnode(3);
 
      p->left = p1;
          p->right = p2;
               p1->left = p3;
                    p2->right = p4;
                    p1->right = p5;
- transverse(p);
-
+ 
+ printf("\n%d \n",isBST(p));
+deleteNode(p,5);
+transverse(p);
     
 //               0
 //          1       2
